b7ebe44ec72311ad28c2d5bd50634480ad392f72
abhaytiwari-top <tiwarisabhay1@gmail.com>
2025-03-19 15:30:24 +0530

    Added Fetch Type in Category Model

--- ProductService/src/main/java/com/example/productservice/Models/Product.java
+++ ProductService/src/main/java/com/example/productservice/Models/Product.java
@@ -17,7 +17,7 @@ public class Product extends BaseModel {
     private String description;
     private String imageURL;
 
-    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JsonIgnore
     private Category category;
 
